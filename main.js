e7a4aa154b33ed8b89dde3759282c88d2a25e4f7
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-06-13 00:24:08 +0900

    ふわふわ文字

--- main.js
+++ main.js
@@ -12,7 +12,19 @@ gsap.from(".box", {
   ease: "power1.inOut",
   stagger: {
     grid: [7,15],
-    from: "end",
+    from: "strat",
     amount: 1.5
   }
 });
+
+// gsap.from(".box", {
+//   duration: 3,
+//   scale: 0.1,
+//   y: 40,
+//   ease: "power1.inOut",
+//   stagger: {
+//     grid: [7,15],
+//     from: "end",
+//     amount: 1.5
+//   }
+// });
